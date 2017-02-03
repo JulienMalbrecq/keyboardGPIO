ce6f50b3230b50ffe46f4bd9f1be95eb34838aea
julmalbr <julien.malbrecq@digitaslbi.com>
2017-02-03 20:21:13 +0100

    Removed test code + wiring of GPIO

--- src/main/java/be/malbrecq/KeyboardGPIO.java
+++ src/main/java/be/malbrecq/KeyboardGPIO.java
@@ -1,9 +1,11 @@
 package be.malbrecq;
 
-import be.malbrecq.utils.ProgramFocus;
 import be.malbrecq.utils.Shortcuts;
 import be.malbrecq.spotify.SpotifyController;
 import be.malbrecq.utils.VirtualKeyboardImpl;
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 import java.awt.*;
 import java.util.logging.Logger;
@@ -11,27 +13,39 @@ import java.util.logging.Logger;
 public class KeyboardGPIO {
     private final static Logger log = Logger.getLogger(KeyboardGPIO.class.getCanonicalName());
 
+    private Shortcuts spotify;
+    private GpioPinDigitalInput piezo;
+
+    public KeyboardGPIO() throws AWTException {
+        final GpioController gpio = GpioFactory.getInstance();
+
+        spotify = new SpotifyController(new VirtualKeyboardImpl(new Robot()));
+        piezo = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+    }
+
+    public void init() {
+        piezo.setShutdownOptions(true);
+
+        // create and register gpio pin listener
+        piezo.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                // display pin state on console
+                log.info(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+
+                if (event.getState().equals(PinState.HIGH)) {
+                    spotify.next();
+                }
+            }
+        });
+    }
+
     public static void main(String[] args) throws InterruptedException, AWTException {
-        // focus on spotify
-        Shortcuts spotify = new SpotifyController(new VirtualKeyboardImpl(new Robot()));
+        KeyboardGPIO key = new KeyboardGPIO();
+        key.init();
 
-        for (int i = 5; i >= 0; i--) {
-             log.info("Starts in " + i);
-            Thread.sleep(1000);
+        while (true) {
+            Thread.sleep(500);
         }
-
-        log.info("Playing song");
-        spotify.playPause();
-        Thread.sleep(5000);
-
-        log.info("Next song");
-        spotify.next();
-        Thread.sleep(5000);
-
-        log.info("Previous song");
-        spotify.previous();
-        Thread.sleep(5000);
-
-        ((ProgramFocus)spotify).quit();
     }
 }
